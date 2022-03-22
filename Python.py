2a83d956edb7f0d8db1bbec305df1f7d638d73d5
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-03-22 14:35:54 -0500

    Commit

--- Python.py
+++ Python.py
@@ -5,3 +5,13 @@ import threading
 import time
 from functools import reduce
 
+
+# If Else Statment
+var = 25
+
+if var > 21:
+    print("Y")
+elif var >= 16:
+    print("N")
+else:
+    print("Negative")
