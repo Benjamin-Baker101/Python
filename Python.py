919773740ab3cf226d20bdbb4a3bb5e27bf70854
Benjamin-Baker101 <63737678+Benjamin-Baker101@users.noreply.github.com>
2022-03-31 22:59:27 -0500

    More Stuff

--- Python.py
+++ Python.py
@@ -4,6 +4,7 @@ import math
 import threading
 import time
 from functools import reduce
+from typing import List
 
 
 # If Else Statment
@@ -23,11 +24,11 @@ aBoolean = True
 aBoolean = True if var >= 25 else False
 print(aBoolean)
 
-# Checking the first 4 characters in the string 
+# Strings
 str_1 = "Strings"
-print("1st chars to 4 \n\n", str_1[0:4])
+print("1st CHARS to 4 \n\n", str_1[0:4])
 
-# Checking for the word is "is" in the string str_1 will print false to the console
+
 print("is" is str_1)
 
 
@@ -45,7 +46,7 @@ print(someRandomString)
 
 print("?" not in someRandomString)
 
-print("You index", someRandomString.find(">>"))
+print("You index", someRandomString.find("A"))
 
 print("        Hello      ".rstrip())
 
@@ -57,3 +58,26 @@ print("A letters".split(" "))
 int1 = int2 = 5
 print(f'{int1}+{int2} = {int1 + int2}')
 
+
+print("A SMALL STRING".lower())
+print("A Big String".upper())
+
+print("ABig String".isalnum())
+print("IsAlpha".isalpha())
+print("124".isdigit())
+
+
+# Lists 
+
+li = [1,2.23,"Ben, True"]
+print("The Length", len(li))
+print("1st", li[0])
+print("Last", li[-1])
+
+
+li[0] = 2
+li[2:4] = ["John", False]
+li[2:2] = ["Jim", 13]
+li.insert(1, 3.14)
+l2 =  li + ["Multimeter" , 4]
+print("l2" + l2)
